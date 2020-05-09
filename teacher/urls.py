791d40b70c0259b67cb1d625006a8c6e14021443
shreytrivedi002 <shreytrivedi002@gmail.com>
2020-05-09 17:32:38 +0530

    home access

--- teacher/urls.py
+++ teacher/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('edclg/', include('edclg.urls'))
+    path('', include('edclg.urls'))
     
 
 ]
