a37036d90b2efa7b08bf623b7f75c6022b502e3c
shreytrivedi002 <shreytrivedi002@gmail.com>
2020-05-18 12:08:56 +0530

    view functionality added

--- edclg/urls.py
+++ edclg/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path , include
 from . import views 
+from django.conf.urls.static import static
+
 
 
 
@@ -38,4 +41,4 @@ urlpatterns = [
     
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- teacher/settings.py
+++ teacher/settings.py
@@ -139,14 +139,14 @@ LOGIN_URL = 'login'
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shreytrivedi002@gmail.com'
-EMAIL_HOST_PASSWORD = 'bzyhodgzilojtbkl'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'shreytrivedi002@gmail.com'
+# EMAIL_HOST_PASSWORD = 'bzyhodgzilojtbkl'
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 django_heroku.settings(locals())
 
 
